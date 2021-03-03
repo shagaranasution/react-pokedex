43542526826ad7563205a80ff18de0acd96b1acf
Shagara Nasution <shagara@ruangguru.com>
2021-03-03 17:00:52 +0700

    Create Pokemon List Data Fetching

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,4 +1,35 @@
+import { useState, useEffect } from 'react'
+import { getPokemons, getPokemon } from '../../services/pokemon'
+
 const Home = () => {
+  const [pokemonData, setPokemonData] = useState([])
+  const [nextUrl, setNextUrl] = useState('')
+  const [prevUrl, setPrevUrl] = useState('')
+  const [isLoading, setIsLoading] = useState(true)
+  const initialURL = 'https://pokeapi.co/api/v2/pokemon'
+
+  useEffect(() => {
+    const fetchData = async () => {
+      let response = await getPokemons(initialURL)
+      
+      await loadPokemon(response.results)
+      setNextUrl(response.next)
+      setPrevUrl(response.previous)
+      setIsLoading(false)
+    }
+    fetchData()
+  }, [])
+
+  const loadPokemon = async (data) => {
+    let pokemonData = await Promise.all(data.map(async (pokemon) => {
+      let pokemonRecord = await getPokemon(pokemon.url)
+
+      return pokemonRecord
+    }))
+    console.log(pokemonData)
+    setPokemonData(pokemonData)
+  }
+
   return (
     <div className="home">
       <h1>
---
+++ src/services/pokemon.js
@@ -0,0 +1,23 @@
+export async function getPokemons(url) {
+  return new Promise((resolve, reject) => {
+    fetch(url)
+      .then(
+        response => response.json()
+      )
+      .then(
+        data => resolve(data)
+      )
+  })
+}
+
+export async function getPokemon(url) {
+  return new Promise((resolve, reject) => {
+    fetch(url)
+      .then(
+        response => response.json()
+      )
+      .then(
+        data => resolve(data)
+      )
+  })
+}
